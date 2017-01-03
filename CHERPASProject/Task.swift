31b229775bfd3a57f950f0430b7413977afa5a3f
Rachel <rachel.l.pavilanis@gmail.com>
2017-01-03 15:31:56 -0800

    added basic model objects and .gitignore file.

---
+++ CHERPASProject/Task.swift
@@ -0,0 +1,46 @@
+//
+//  Task.swift
+//  CHERPASProject
+//
+//  Created by Rachel Pavilanis on 1/3/17.
+//  Copyright © 2017 Rachel Pavilanis. All rights reserved.
+//
+
+import Foundation
+
+import RealmSwift
+
+// Model
+
+final class Category: Object {
+    dynamic var name = ""
+//    dynamic var id = ""
+    let tasks = List<DailyTask>()
+    let monthlyGoals = List<MonthlyGoal>()
+    let yearlyGoals = List<YearlyGoal>()
+    
+//    override static func primaryKey() -> String? {
+//        return "id"
+//    }
+}
+
+final class DailyTask: Object {
+    dynamic var name = ""
+    dynamic var createdAt = NSDate()
+    dynamic var isCompleted = false
+}
+
+final class MonthlyGoal: Object {
+    dynamic var desc = ""
+    dynamic var createdAt = NSDate()
+    dynamic var isCompleted = false
+}
+
+
+final class YearlyGoal: Object {
+    dynamic var desc = ""
+    dynamic var createdAt = NSDate()
+    dynamic var isCompleted = false
+}
+
+
