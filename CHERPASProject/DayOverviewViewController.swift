e66c26ef386795ff92d420b9adbe7223aa665673
Rachel <rachel.l.pavilanis@gmail.com>
2017-01-11 09:55:28 -0800

    program is working at this point - committing changes because I am about to work on refactoring tasks and I want to be able to reset if needed.

--- CHERPASProject/DayOverviewViewController.swift
+++ CHERPASProject/DayOverviewViewController.swift
@@ -14,8 +14,6 @@ class DayOverviewViewController: UITableViewController {
     
     let section = ["Daily Task", "Monthly Goal", "Yearly Goal", "Quote of the Day"]
 
-    let items = [["Clean the kitchen"], ["Clean for 15 minutes each day."], ["At monthly check-ins, the apartment will be clean and organized due to gradual daily cleaning."], ["Our goals can only be reached through a vehicle of a plan, in which we must fervently believe and upon which we must vigorously act. There is no other route to success."]]
-
     
     // stores dailyTask information passed from category view
     var sentData1:String!
@@ -95,7 +93,7 @@ class DayOverviewViewController: UITableViewController {
         switch (indexPath.section)
         {
         case 0:
-            cell.textLabel?.text = dailyTask[indexPath.row]
+            cell.textLabel?.text = sentData1
         case 1:
             cell.textLabel?.text = sentData2
         case 2:
@@ -116,69 +114,26 @@ class DayOverviewViewController: UITableViewController {
         header.textLabel?.textColor = UIColor(red: 27/255, green: 124/255, blue: 150/255, alpha: 1.0)
     }
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-    
     override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
 //        super.tableView(tableView, editActionsForRowAt: indexPath)
         
         let delete = UITableViewRowAction(style: .normal, title: "Delete") { action, index in
 //            print("delete button tapped")
-            let todayStart = Calendar.current.startOfDay(for: Date() as Date)
-            let todayEnd: Date = {
-                var components = DateComponents()
-                components.day = 1
-                components.second = -1
-                return Calendar.current.date(byAdding: components, to: todayStart)!
-            }()
+//            let todayStart = Calendar.current.startOfDay(for: Date() as Date)
+//            let todayEnd: Date = {
+//                var components = DateComponents()
+//                components.day = 1
+//                components.second = -1
+//                return Calendar.current.date(byAdding: components, to: todayStart)!
+//            }()
             let realm = try! Realm()
-            let allTasks = realm.objects(DailyTask)
-            let currentTask = allTasks.filter("createdAt BETWEEN %@", [todayStart, todayEnd])
+            let selectedTask = realm.objects(DailyTask.self)[indexPath.row]
+//            let currentTask = allTasks.filter("createdAt BETWEEN %@", [todayStart, todayEnd])
 //            currently this works to delete targeted object, HOWEVER - then my array shifts and things move around.
             try! realm.write {
-                realm.delete(currentTask[indexPath.row])
+                realm.delete(selectedTask)
+                let paths = [indexPath]
+                self.tableView.reloadRows(at: paths, with: UITableViewRowAnimation.none)
             }
         }
         delete.backgroundColor = UIColor(red: 27/255, green: 124/255, blue: 150/255, alpha: 1.0)
@@ -191,10 +146,27 @@ class DayOverviewViewController: UITableViewController {
         // need to adjust this with if statement once add in model - if task is complete already, need to be able to change to incomplete
         let markComplete = UITableViewRowAction(style: .normal, title: "Complete") { action, index in
             print(indexPath)
-//            let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: message)
-//            attributeString.addAttribute(NSStrikethroughStyleAttributeName, value: 2, range: NSMakeRange(0, attributeString.length))
-//            
-//            cell.textLabel?.attributedText =  attributeString
+//            let todayStart = Calendar.current.startOfDay(for: Date() as Date)
+//            let todayEnd: Date = {
+//                var components = DateComponents()
+//                components.day = 1
+//                components.second = -1
+//                return Calendar.current.date(byAdding: components, to: todayStart)!
+//            }()
+//            let realm = try! Realm()
+//            let allTasks = realm.objects(DailyTask)
+//            let currentTask = allTasks.filter("createdAt BETWEEN %@", [todayStart, todayEnd])
+//            //            currently this works to delete targeted object, HOWEVER - then my array shifts and things move around.
+//            try! realm.write {
+//                let attributeString: NSMutableAttributedString =  NSMutableAttributedString(string: message)
+//                attributeString.addAttribute(NSStrikethroughStyleAttributeName, value: 2, range: NSMakeRange(0, attributeString.length))
+//    
+//                cell.textLabel?.attributedText =  attributeString
+//            }
+//
+            
+            
+
             //remove strikethrough
             // cell.textLabel?.attributedText =  nil
             
@@ -250,5 +222,4 @@ class DayOverviewViewController: UITableViewController {
         
     }
     
-
 }
