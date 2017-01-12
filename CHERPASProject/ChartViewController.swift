2ce49b6166339da29c3ab1acad983c8bd3cb6287
Rachel <rachel.l.pavilanis@gmail.com>
2017-01-11 22:01:22 -0800

    working on chart view - coming along with code and fake data - will add Realm to chart once I have it working

--- CHERPASProject/ChartViewController.swift
+++ CHERPASProject/ChartViewController.swift
@@ -13,11 +13,33 @@ import RealmSwift
 class ChartViewController: UIViewController {
     
     @IBOutlet weak var setChart: BarChartView!
+    var months: [String]!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        let percentComplete = [10, 70, 25, 60, 67, 50, 60, 70, 80, 90, 100]
+        
+        makeChart(dataPoints: months, values: percentComplete)
         //        updateChartWithData()
         // Do any additional setup after loading the view.
+    }
+    
+ 
+    func makeChart(dataPoints: [String], values: [Int]) {
+        setChart.noDataText = "You don't yet have enough data to produce your weekly chart."
+        
+        var dataEntries: [BarChartDataEntry] = []
+        
+        for i in 0..<dataPoints.count {
+            let dataEntry = BarChartDataEntry(x: Double(i), y: Double(dataPoints[i].count))
+            dataEntries.append(dataEntry)
+        }
+        
+        let chartDataSet = BarChartDataSet(values: dataEntries, label: "Percentage Completed in last Week")
+        let chartData = BarChartData(dataSet: chartDataSet)
+        //outlet from view 
+        setChart.data = chartData
     }
     
    
