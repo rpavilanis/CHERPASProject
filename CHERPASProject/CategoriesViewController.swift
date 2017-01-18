0104cd9fafc41b59d9b95fe1303b7995de4f8d23
Rachel <rachel.l.pavilanis@gmail.com>
2017-01-18 11:00:35 -0800

    fixed a bug so that my monthly goals show from 30 days prior to the end of the current date instead of the beginning.

--- CHERPASProject/CategoriesViewController.swift
+++ CHERPASProject/CategoriesViewController.swift
@@ -170,7 +170,16 @@ class CategoriesViewController: UITableViewController {
     
     func queryGoals() {
         
-        let monthEnd = Calendar.current.startOfDay(for: Date() as Date)
+        let startOfDay = Calendar.current.startOfDay(for: Date() as Date)
+
+        
+        let monthEnd: Date = {
+            var components = DateComponents()
+            components.day = 1
+            components.second = -1
+            return Calendar.current.date(byAdding: components, to: startOfDay)!
+        }()
+        
         let monthStart: Date = {
             var components = DateComponents()
             components.day = -30
@@ -260,3 +269,5 @@ class CategoriesViewController: UITableViewController {
     }
 
 }
+
+
